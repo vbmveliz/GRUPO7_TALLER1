cc935fe6cafa930aa1db910b142c135b36114e83
Janina <N00415221@upn.pe>
2026-04-11 18:40:28 -0500

    Creación de objeto Persona y uso de main

---
+++ GRUPO7_TALLER1/src/CASO_2/Resolucion.java
@@ -0,0 +1,12 @@
+package CASO_2;
+
+public class Resolucion {
+	public static void main(String[] args) {
+
+	    Persona p1 = new Persona("Ana", 20);
+	    Persona p2 = new Persona("Luis", 25);
+
+	    p1.mostrarDatos();
+	    p2.mostrarDatos();
+	}
+}
--- GRUPO7_TALLER1/src/CASO_2/resolucion.java
+++
@@ -1,5 +0,0 @@
-package CASO_2;
-
-public class resolucion {
-
-}
