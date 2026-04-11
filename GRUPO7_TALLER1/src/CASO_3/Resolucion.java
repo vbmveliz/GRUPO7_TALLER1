0d1df95a79ebe1710fc92bd51b9aba16d9e37142
ManuelCarbajalG <Usuario@SERVER>
2026-04-11 16:22:37 -0500

    uLTIMO Cambio

--- GRUPO7_TALLER1/src/CASO_3/Resolucion.java
+++ GRUPO7_TALLER1/src/CASO_3/Resolucion.java
@@ -1,5 +1,18 @@
 package CASO_3;
 
 public class Resolucion {
+    public static void main(String[] args) {
+        Operaciones op = new Operaciones();
 
+        int sumad2Enteros = op.sumar(10, 20);
+        double sumad2Doubles = op.sumar(15.5, 4.3);
+        int sumad3Enteros = op.sumar(5, 10, 15);
+
+        System.out.println("--- Resultados de Sobrecarga ---");
+        System.out.println("Suma de 2 enteros (10 + 20): " + sumad2Enteros);
+        System.out.println("Suma de 2 doubles (15.5 + 4.3): " + sumad2Doubles);
+        System.out.println("Suma de 3 enteros (5 + 10 + 15): " + sumad3Enteros);
+    }
 }
+
+
