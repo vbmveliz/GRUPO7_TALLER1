607eb7709baeb2936c24f81f7a8bda7079b35167
Janina <N00415221@upn.pe>
2026-04-11 18:16:27 -0500

    Agregando atributos y constructor a la clase persona

--- GRUPO7_TALLER1/src/CASO_2/Persona.java
+++ GRUPO7_TALLER1/src/CASO_2/Persona.java
@@ -1,5 +1,11 @@
 package CASO_2;
 
 public class Persona {
-
+	private String nombre;
+	private int edad;
+	
+public Persona(String nombre, int edad) {
+	this.nombre = nombre;
+	this.edad = edad;
+}
 }
