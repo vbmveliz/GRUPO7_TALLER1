9d489e8d776835979a26e46df77a907c49c87bea
Janina <N00415221@upn.pe>
2026-04-11 23:03:36 -0500

    Desarrollo de caso 2: clase Persona y clase Resolucion en Java

--- src/CASO_2/Persona.java
+++ src/CASO_2/Persona.java
@@ -4,8 +4,12 @@ public class Persona {
 	private String nombre;
 	private int edad;
 	
-public Persona(String nombre, int edad) {
+	public Persona(String nombre, int edad) {
 	this.nombre = nombre;
 	this.edad = edad;
-}
+	}
+	public void mostrarDatos() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+    }
 }
--- src/CASO_2/resolucion.java
+++ src/CASO_2/resolucion.java
@@ -1,5 +1,15 @@
 package CASO_2;
 
 public class resolucion {
+	public static void main(String[] args) {
 
+	    Persona p1 = new Persona("Ana", 20);
+	    Persona p2 = new Persona("Luis", 25);
+
+	    System.out.println("----- Persona 1 -----");
+        p1.mostrarDatos();
+
+        System.out.println("----- Persona 2 -----");
+        p2.mostrarDatos();
+	}
 }
