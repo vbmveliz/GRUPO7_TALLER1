d7a66a6312beb3920019110e47f1abd86865db24
Janina <N00415221@upn.pe>
2026-04-11 19:01:54 -0500

    Mejora formato salida en Consola

--- GRUPO7_TALLER1/src/CASO_2/Resolucion.java
+++ GRUPO7_TALLER1/src/CASO_2/Resolucion.java
@@ -3,10 +3,13 @@ package CASO_2;
 public class Resolucion {
 	public static void main(String[] args) {
 
-	    Persona p1 = new Persona("Ana", 20);
+	    Persona p1 = new Persona("Janina", 27);
 	    Persona p2 = new Persona("Luis", 25);
 
-	    p1.mostrarDatos();
-	    p2.mostrarDatos();
+	    System.out.println("----- Persona 1 -----");
+        p1.mostrarDatos();
+
+        System.out.println("----- Persona 2 -----");
+        p2.mostrarDatos();
 	}
 }
